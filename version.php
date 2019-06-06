3fe5d4efa72703955f54d9fb5b19a7de189549c9
satoshi-yazawa-nii <satoshi-yazawa@nii.ac.jp>
2019-06-07 07:34:41 +0900

    Bump the version

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
 
-$OC_Version = array(12, 0, 6, 3);
+$OC_Version = array(12, 0, 6, 4);
 
 // The human readable string
 $OC_VersionString = '12.0.6';
